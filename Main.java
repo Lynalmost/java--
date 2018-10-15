19192620affbd90602ed90e5bc15536bbdbced80
Lynalmost <33012209+Lynalmost@users.noreply.github.com>
2018-10-15 15:36:02 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+/**
+ * @author Lyn
+ * Main.java && Manu.java 
+ * 版本 1 2018.9.10	实现基本菜单功能
+ * 版本 2 2018.9.25	实现用户注册功能，新建User类用于存放用户信息
+ * 版本 3 2018.9.27	实现用户登录&退出功能
+ * 版本 4 2018.9.28	实现用户找回密码功能，验证码功能
+ * 实现菜单功能
+ */
+package yin.li.second.program;
+public class Main {
+	public static void main(String[] args) {
+		Menu.mainMenu();
+	}
+
+}
